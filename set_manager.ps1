2636601034066ad3d9f44954d7844adeaafa14ec
Jesse Ludeman <JLudeman@royalfreemasons.org.au>
2018-08-08 12:20:12 +1000

    Second commit

---
+++ set_manager.ps1
@@ -0,0 +1,14 @@
+﻿function Set-Manager()
+{
+    Write-Host '***This will copy the Direct Reports from a terminated Manager to a new Manager***'
+
+    $OldManager = Read-Host 'Enter a manager to copy from'
+    $ManagerData = Get-ADUser -Identity $OldManager -Properties DirectReports | Select-Object -ExpandProperty DirectReports
+    $NewManager = Read-Host 'Enter a new manager to copy to'
+        
+    foreach ($DirectReport in $ManagerData)
+    {
+    # Copy the Direct Reports across to the new user
+    Get-ADUser -Identity $DirectReport | Set-ADUser -Manager $NewManager
+    }
+}
